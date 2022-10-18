2020c22249d731a8755b9c3a58a2103274b69ad7
Amber <abrands1993@gmail.com>
2022-10-18 16:16:58 +0200

    add visualizations

--- models/utils.py
+++ models/utils.py
@@ -34,8 +34,6 @@ def h2(tau2, sample_rate, t_steps, b, c, w, h):
 
 def torch_convolve(x, y, b, c, w, h, n, t_steps):
 
-    # fig, axs = plt.subplots(1, t_steps, figsize=(10, 2))
-
     # preprocess
     x_resh = x.reshape(t_steps, b*c*w*h)
     y_resh = y.reshape(t_steps, b*c*w*h)
@@ -61,9 +59,7 @@ def torch_cross_val(x, y, b, c, w, h, n, t_steps):
     y_resh = y.reshape(t_steps, b*c*w*h)
 
     # fix stimulus
-    x_tau = torch.zeros(t_steps*2, b*c*w*h)
-    x_tau[t_steps:t_steps*2, :] = x_resh
-    print(x_tau.shape)
+    x_tau = F.pad(x_resh, [0, 0, t_steps, 0])
 
     output = torch.Tensor(t_steps, b*c*w*h)
     for t in range(t_steps):
@@ -76,28 +72,31 @@ def torch_cross_val(x, y, b, c, w, h, n, t_steps):
         # sliding dot product
         output[t, :] = torch.tensordot(x_tau, y_shift)
 
-
     return output.reshape(t_steps, b, c, w, h)
 
-def torch_convolve_norm(x, y, b, c, w, h, sigma, n, t_steps, t_current):
+def torch_cross_val_norm(x, y, b, c, w, h, sigma, n, t_steps):
 
     # preprocess
     x_resh = x.reshape(t_steps, b*c*w*h)
     y_resh = y.reshape(t_steps, b*c*w*h)
 
-    # remake irf
-    y_shift = torch.zeros(t_steps, b*c*w*h)
-    y_shift[-t_current:, :] = y_resh[:t_current, :] 
+    # fix stimulus
+    x_tau = F.pad(x_resh, [0, 0, t_steps, 0])
 
     # reshape sigma
     sigma_resh = sigma.reshape(c*w*h).repeat(b)
-    n_resh = n.reshape(c*w*h).repeat(b)
 
-    # sliding dot product
-    cv = torch.zeros(b*c*w*h)
-    for i in range(b*c*w*h):
-        cv[i] = torch.dot(x_resh[:, i], y_shift[:, i])
+    convnl = torch.Tensor(t_steps, b*c*w*h)
+    normrsp = torch.Tensor(t_steps, b*c*w*h)
+    for t in range(t_steps):
+        if t == 0:
+            continue
 
-    normrsp = torch.abs(cv)**n_resh + sigma_resh**n_resh
+        # add padding
+        y_shift = F.pad(y_resh, [0, 0, t, t_steps-t])
 
-    return cv.reshape(b, c, w, h), normrsp.reshape(b, c, w, h)
+        # sliding dot product
+        convnl[t, :] = torch.tensordot(x_tau, y_shift)
+        normrsp[t, :] = torch.add(convnl[t, :], sigma_resh)
+
+    return convnl.reshape(t_steps, b, c, w, h), normrsp.reshape(t_steps, b, c, w, h)
